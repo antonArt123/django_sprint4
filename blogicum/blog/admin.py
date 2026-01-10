74eab545599a32cc223a2b1008548a30590ed398
antonArt123 <anton.art123@yandex.ru>
2026-01-10 19:37:54 +0300

    Delete blogicum directory

--- blogicum/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Category, Location, Post
-
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
